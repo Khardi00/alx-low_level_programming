a3ed29edb334661c9976e085239a0a600dd02f41
khardi00 <khadijahgarba2017@gmail.com>
2023-09-08 05:49:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * man - Entry point
+ * main - Entry point
  *
- * REturn Always 0 (Success)
+ * Return: Always 0
  */
+
 int main(void)
 (
-    puts("\"programming is like building a multilingual puzzle");
-    return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 )
