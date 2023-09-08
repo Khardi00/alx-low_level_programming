f614ba09878412eaa8ffe75274aa354702228c45
khardi00 <khadijahgarba2017@gmail.com>
2023-09-08 03:17:16 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * man - Entry point
+ *
+ * REturn Always 0 (Success)
+ */
+int main(void)
+(
+    puts("\"programming is like building a multilingual puzzle");
+    return (0);
+)
