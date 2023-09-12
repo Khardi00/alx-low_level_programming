428a66f6fbd163e7411669371993b8eacecc459c
khardi00 <khadijahgarba2017@gmail.com>
2023-09-12 13:39:50 -0700

    add two variable and return their values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two inetger and returns the function
+ * @a: parameter checked
+ * @b: parameter checked
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a  + b);
+}
