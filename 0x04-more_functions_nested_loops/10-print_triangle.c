95d7bfafe82ecce0c195d5d71cbc5588a74a789d
khardi00 <khadijahgarba2017@gmail.com>
2023-09-14 14:23:07 -0700

    a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,23 +8,23 @@
 
 void print_triangle(int size)
 {
-	int i, j;
-
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (i = 1; i < size; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			for (j = size - i; j > 0; j--)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = 0; j < i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
-			}
-			if (i == size)
-			{
-				continue;
 			}
 			_putchar('\n');
 		}
