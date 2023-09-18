5d7a9d739526a3e349f8f0b325b0977a6e54eaf3
khardi00 <khadijahgarba2017@gmail.com>
2023-09-18 07:41:48 -0700

    a function thatprintsevery other characterofa string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that print every other character of astring
+ * @str: parameter checked
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int num;
+
+	for (num = 0; str[num] != '\0'; num++)
+	{
+		if (num % 2 == 0)
+			_putchar(str[num]);
+		_putchar('\n');
+	}
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void swap_int(int *a, int *b);
  int _putchar(char c);
  void print_rev(char *s);
  void rev_string(char *s);
+ void puts2(char *str);
 
 #endif
