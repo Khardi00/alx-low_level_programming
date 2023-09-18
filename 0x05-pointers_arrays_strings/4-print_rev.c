2846b654d183af2ae65cb7adf196dd8cd15f2de0
khardi00 <khadijahgarba2017@gmail.com>
2023-09-18 06:54:25 -0700

    a function that writes integers in reverse form

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print numbers in reverse format
+ * @s: parameter checked
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int num;
+	int lenght;
+
+	for (num = 0; s[num] != '\0'; num--)
+		lenght--;
+	for (num = lenght - 1; num >= 0; num--)
+		_putchar(s[num]);
+	_putchar('\n');
+}
