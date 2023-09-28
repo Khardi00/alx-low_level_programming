9c64f42087547e869dcd6a769e05c17d03d37a5d
khardi00 <khadijahgarba2017@gmail.com>
2023-09-28 08:34:21 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	while (*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
