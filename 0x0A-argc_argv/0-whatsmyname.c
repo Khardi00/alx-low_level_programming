feb52afb0000df2f9f48edf241ceb4c04f594e88
khardi00 <khadijahgarba2017@gmail.com>
2023-10-02 15:32:26 -0700

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - a function that prints num of argument
+ * main - a function that prints name of program
  * @argc: argument oarameter
  * @argv: argument
  * Return: 0
@@ -9,8 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
-
-	printf("%d\n", argc - 1);
+	(void) argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
