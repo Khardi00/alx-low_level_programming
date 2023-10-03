4cbcf650a6f9564168eba4de53f1ea0e2ebc3169
khardi00 <khadijahgarba2017@gmail.com>
2023-10-03 13:12:59 -0700

    a function that add positive integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that  adds positive number of integers
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int k, l;
+	int sum = 0;
+
+	if (argc == 1)
+		printf("%d\n", 0);
+	else
+	{
+		for (k = 0; k < argc; k++)
+		{
+			for (l = 1; argv[k][l] != '\0'; l++)
+			{
+				if (!(isdigit(argv[k][k])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			 sum = sum + atoi(argv[k]);
+		}		
+		printf("%d\n", sum);
+	}
+	return (0);
+}
