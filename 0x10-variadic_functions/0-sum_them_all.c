e66105c0b49fdd2ce8ad6003f9e0c0a1e8279276
khardi00 <khadijahgarba2017@gmail.com>
2023-10-12 11:28:36 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - a function that returns the sum of all integers
  * @n: parameter
- * Return: sum of the number
+ * Return: 0 if n == 0
  */
 
 int sum_them_all(const unsigned int n, ...)
